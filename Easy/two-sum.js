a32f935d3a391c8b119ac5c2c469966467519219
Asad <syedasadabrar@gmail.com>
2022-11-02 23:24:33 +0500

    two-sum done

--- Easy/two-sum.js
+++ Easy/two-sum.js
@@ -1,1 +1,21 @@
 // Problem Link: https://leetcode.com/problems/two-sum/
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  const answer = [];
+  nums.some((num, index, arr) => {
+    if (
+      arr.includes(target - num) &&
+      !answer.includes(index) &&
+      arr.indexOf(target - num) !== index
+    ) {
+      answer.push(index);
+      answer.push(arr.indexOf(target - num));
+      return true;
+    }
+  });
+  return answer;
+};
