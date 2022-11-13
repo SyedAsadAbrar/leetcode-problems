e787d2ce8a62e221c402a6dcd9fdd1652e2f2bef
Asad <syedasadabrar@gmail.com>
2022-11-13 06:21:38 +0500

    palindrome number added

--- Easy/palindrome-number.js
+++ Easy/palindrome-number.js
@@ -11,24 +11,15 @@ var isPalindrome = function (x) {
     return false;
   }
   var num = x;
-  const list = [];
+  const reverseNum = 0;
   var temp = 0;
   var forward = 0;
   var backward = 0;
   while (num > 0) {
     temp = (((num / 10).toFixed(1) - (num / 10).toFixed(0)) * 10).toFixed(0);
-    list.push(temp);
+    reverseNum = reverseNum * 10 + temp;
     num = parseInt(num / 10);
   }
 
-  backward = list.length - 1;
-
-  while (forward < backward) {
-    if (list[forward] !== list[backward]) {
-      return false;
-    }
-    forward += 1;
-    backward -= 1;
-  }
-  return true;
+  return reverseNum === x;
 };
