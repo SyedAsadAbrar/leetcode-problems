847fc04191d9896c312bb7612e7b7eb00b30620b
Syed Asad Abrar <syedasadabrar@gmail.com>
2025-06-23 15:57:27 +0400

    added word-omitter

---
+++ Hard/react-word-omitter.js
@@ -0,0 +1,60 @@
+// https://www.hackerrank.com/challenges/react-word-omitter/problem
+// React
+
+import React, { useState } from "react";
+
+const OMITTED_WORDS = ["a", "the", "and", "or", "but"];
+
+function WordOmitter() {
+  const [inputText, setInputText] = useState("");
+  const [omitWords, setOmitWords] = useState(true);
+
+  const handleInputChange = (e) => {
+    setInputText(e.target.value);
+  };
+
+  const toggleOmitWords = () => {
+    setOmitWords(!omitWords);
+  };
+
+  const clearFields = () => {
+    setInputText("");
+  };
+
+  const getProcessedText = () => {
+    if (inputText.length) {
+      let outputText = inputText;
+      omitWords &&
+        OMITTED_WORDS.forEach((word) => {
+          outputText = outputText.replaceAll(` ${word} `, " ");
+        });
+      return outputText;
+    }
+    return "";
+  };
+
+  return (
+    <div className="omitter-wrapper">
+      <textarea
+        placeholder="Type here..."
+        value={inputText}
+        onChange={handleInputChange}
+        data-testid="input-area"
+      />
+      <div>
+        <button onClick={toggleOmitWords} data-testid="action-btn">
+          {omitWords ? "Show All Words" : "Omit Words"}
+        </button>
+        <button onClick={clearFields} data-testid="clear-btn">
+          Clear
+        </button>
+      </div>
+      <div>
+        <h2>Output:</h2>
+        <p data-testid="output-text">{getProcessedText()}</p>
+      </div>
+    </div>
+  );
+}
+
+export { WordOmitter };
