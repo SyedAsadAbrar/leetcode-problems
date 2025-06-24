94ac310526a1b8a69fbc24781a6a9089685096f6
Syed Asad Abrar <syedasadabrar@gmail.com>
2025-06-25 03:46:24 +0400

    merge-sorted-array done

--- Easy/merge-sorted-array.ts
+++ Easy/merge-sorted-array.ts
@@ -4,15 +4,15 @@
  Do not return anything, modify nums1 in-place instead.
  */
 function merge(nums1: number[], m: number, nums2: number[], n: number): void {
-  let i = 0;
+  let i = nums1.length - 1;
   let j = 0;
 
-  while (i < nums1.length) {
+  while (j < nums2.length) {
     if (nums1[i] === 0) {
       nums1[i] = nums2[j];
       j++;
     }
-    i++;
+    i--;
   }
 
   nums1.sort((a, b) => a - b);
