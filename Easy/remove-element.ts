1334bf6279e59fe686a63bd29593a66167536c5d
Syed Asad Abrar <syedasadabrar@gmail.com>
2025-06-25 01:33:19 +0400

    updated

--- Easy/remove-element.ts
+++ Easy/remove-element.ts
@@ -3,10 +3,20 @@
 function removeElement(nums: number[], val: number): number {
   let counter = 0;
   for (let i = 0; i < nums.length; i++) {
-    if (nums[i] !== val) {
-      nums[counter] = nums[i];
+    if (nums[i] === val) {
+      nums[i] = -1;
+    } else {
       counter++;
     }
   }
+  nums.sort((a, b) => {
+    if (a === -1 && b !== -1) {
+      return 1;
+    }
+    if (a !== -1 && b === -1) {
+      return -1;
+    }
+    return 0;
+  });
   return counter;
 }
